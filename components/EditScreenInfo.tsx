fcd5a23850edbd917669d665fa4c605db799e698
lucasaugsue <lucasaugsue7@gmail.com>
2022-01-29 14:22:12 -0300

    screen home

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -13,7 +13,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          - Informações padrões - 
         </Text>
 
         <View
@@ -26,7 +26,8 @@ export default function EditScreenInfo({ path }: { path: string }) {
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
+          darkColor="rgba(255,255,255,0.8)"
+        >
           Change any of the text, save the file, and your app will automatically update.
         </Text>
       </View>
